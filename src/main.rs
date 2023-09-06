996835b7c5b5db65bd71c510e16b08d279eb91b4
Abhijit <abhijit@pop-os.localdomain>
2023-09-06 13:38:51 +0530

    add: write archive

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod config;
 
 use config::Config;
+use std::error::Error;
+use std::fs::File;
 use std::{env, process};
+use tar::Builder;
 
 fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|e| {
@@ -9,5 +12,21 @@ fn main() {
         process::exit(1);
     });
 
-    dbg!(config);
+    if let Err(e) = run(&config) {
+        eprintln!("[ERROR]: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    if config.options.contains(&'c') {
+        let file = File::create(&config.archive_file)?;
+        let mut a = Builder::new(file);
+
+        for path in &config.paths {
+            a.append_path(path)?;
+        }
+    }
+
+    Ok(())
 }

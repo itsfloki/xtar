f0bca044932b1e60214d453032239e8860f4c7c8
itsfloki <coder.abhijit90@gmail.com>
2023-09-06 08:55:20 +0530

    add: argument parsing

---
+++ src/config.rs
@@ -0,0 +1,35 @@
+#[derive(Debug)]
+pub struct Config {
+    pub options: Vec<char>,
+    pub archive_file: String,
+    pub paths: Vec<String>,
+}
+
+impl Config {
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Self, &'static str> {
+        let mut paths: Vec<String> = Vec::new();
+        args.next();
+
+        let options = match args.next() {
+            Some(op) => op.chars().skip(1).collect::<Vec<char>>(),
+            None => return Err("options arguments missing..."),
+        };
+
+        let archive_file = match args.next() {
+            Some(f) => f,
+            None => return Err("archive_file arguments missing..."),
+        };
+
+        for arg in args {
+            paths.push(arg);
+        }
+
+        Ok(Config {
+            options,
+            archive_file,
+            paths,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod config;
+
+use config::Config;
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args()).unwrap_or_else(|e| {
+        eprintln!("Problem parsing arguments: {}", e);
+        process::exit(1);
+    });
+
+    dbg!(config);
 }
